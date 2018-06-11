94c1bf46f6076904cc4a76aa6ef51c657867a320
ekeilholz+git@outlook.com <ekeilholz+git@outlook.com>
2018-06-11 12:06:26 +0200

    Added a friend

---
+++ NetworkService/DomainModels/Friend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HexMaster.Parcheesi.Common.Base;
+
+namespace HexMaster.Parcheesi.NetworkService.DomainModels
+{
+    public class Friend : DomainModelBase<Guid>
+    {
+        public Guid UserId { get; private set; }
+        public Guid FriendId { get; private set; }
+        public bool IsAccepted { get; private set; }
+        public bool IsPending { get; private set; }
+        public DateTime CreatedOn { get; private set; }
+        public DateTime ExpiresOn { get; private set; }
+        public DateTime? AcceptedOn { get; private set; }
+        public DateTime? DeniedOn { get; private set; }
+    }
+}
