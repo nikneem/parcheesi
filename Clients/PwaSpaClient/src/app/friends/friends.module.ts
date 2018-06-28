6a7a408183010beea411767a7eef1c6baa192853
e.keilholz@bexter.nl <e.keilholz@bexter.nl>
2018-06-28 23:24:29 +0200

    Added theme and styling made it fucking beautiful

---
+++ Clients/PwaSpaClient/src/app/friends/friends.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FriendslistComponent } from './friendslist/friendslist.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [FriendslistComponent]
+})
+export class FriendsModule { }
---
+++ Clients/PwaSpaClient/src/app/games/games.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GameslistComponent } from './gameslist/gameslist.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [GameslistComponent]
+})
+export class GamesModule { }
